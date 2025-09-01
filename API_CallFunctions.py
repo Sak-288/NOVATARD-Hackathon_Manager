f0958ec9164232b3f07625a83b1a7b6839ee318c
Amine Sakoute <aminesakoute288@gmail.com>
2025-09-01 13:49:28 +0100

    Successfully added html content for gmail

--- API_CallFunctions.py
+++ API_CallFunctions.py
@@ -33,11 +33,11 @@ TABLE_ID = "tblt7GgXQ6gCYCV2l"
 # Declaring the Participants Spreadsheet
 daydream_table = api.table(BASE_ID, TABLE_ID)
 
-plain_bodies={'NOT_SIGNED':f"""Hi there {fullname} ! 
+plain_bodies={'NOT_SIGNED':f"""Hi there ! 
 Hope you're doing great. Just a friendly reminder to sign the NDA in the Hackclub form. If you don't, then you won't be able to participate to the evenement, and that would be sad. 
 Best regards, Amine Sakoute | Organizer @Daydream Casablanca 2025.
 
-CLICK on the attachment below !""", 'SIGNED_NDA':f"""Hi there {fullname} ! 
+CLICK on the attachment below !""", 'SIGNED_NDA':f"""Hi there ! 
 Hope you're doing great. Present the QR code below at Ecole Centrale's entrance to get in and participate in Daydream Casablanca 2025 ! Looking forward to seeing you there !
 Best regards, Amine Sakoute | Organizer @Daydream Casablanca 2025."""}
 
@@ -46,6 +46,7 @@ html_bodies={'NOT_SIGNED':"""
              <html lang="en">
              <head>
              <meta charset='utf-8'>
+             </head>
              <style>
              body {
              background-color:lightgreen;
@@ -54,12 +55,33 @@ html_bodies={'NOT_SIGNED':"""
              font-size:20px;
              }
              </style>
-             </head>
              <body>
              <div id="main_div">
              <h1>Hello Bitch !</h1>
-             <p>Hope you're doing okay !</p>
-             <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fabcnews.go.com%2FGMA%2FFamily%2Fnewborn-babys-photo-shoot-viral-hilarious-grumpy-expression%2Fstory%3Fid%3D110522897&psig=AOvVaw3KxRTdySMcFWV6WbeaxsN6&ust=1756816750903000&source=images&cd=vfe&opi=89978449&ved=0CBUQjRxqFwoTCIjRx8HKt48DFQAAAAAdAAAAABAE">
+             <p>SIGN THE FUCKING NDA !</p>
+             <img src="https://i.abcnewsfe.com/a/ffe8d7cb-a13f-4e4e-a45b-0b16f5731915/baby-4-ht-jt-240523_1716503358143_hpEmbed_3x2.jpg">
+             </div>
+             </body>
+             </html>""",
+             'SIGNED_NDA':"""
+             <!DOCTYPE html>
+             <html lang="en">
+             <head>
+             <meta charset='utf-8'>
+             </head>
+             <body>
+             <style>
+             body {
+             background-color:lightgreen;
+             color:black;
+             font-family:'Helvetica', sans-serif;
+             font-size:20px;
+             }
+             </style>
+             <div id="main_div">
+             <h1>Hello Bitch !</h1>
+             <p>CONGRATULATIONS</p>
+             <img src="https://i.pinimg.com/736x/6f/23/67/6f236786f6706f6d978b4de83aa769a5.jpg">
              </div>
              </body>
              </html>"""}
