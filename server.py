ca0b26cf9b0cb2ef1dfa2842f28af228041b0b99
Amine Sakoute <aminesakoute288@gmail.com>
2025-09-16 20:07:36 +0100

    added favicon + div in loading page

--- server.py
+++ server.py
@@ -9,14 +9,17 @@ AIRTABLE_API_KEY = os.environ.get("AIRTABLE_TEST_API_KEY")
 HTML_TEMPLATE = """
 <!DOCTYPE html>
 <html>
-<head><title>Participant Info</title></head>
-<body style="text-align:center;font-weight:bold;color:black;align-items:flex-start; justify-content:space-between; background:radial-gradient(circle at top left,#c0e5fb 0%,#fbecc6 100%) top left,radial-gradient(circle at top right,#c0e5fb 0%,#fbecc6 100%) top right,radial-gradient(circle at bottom left,#c0e5fb 0%,#fbecc6 100%) bottom left,radial-gradient(circle at bottom right,#c0e5fb 0%,#fbecc6 100%) bottom right; background-repeat:repeat; color:black; font-family:'Helvetica',sans-serif; font-size:16px;">
+<head><title>Participant Info</title><link rel="icon" type="image/png" href="https://www.dropbox.com/scl/fi/9tyxp466h915yhionvuxw/cat.ico?rlkey=tsxs01aqis0fs5nz8o0wtv7dx&e=2&st=66xk2y9t&raw=1"></head>
+<body style="position:relative;text-align:center;font-weight:bold;color:black;align-items:flex-start; justify-content:space-between; background:radial-gradient(circle at top left,#c0e5fb 0%,#fbecc6 100%) top left,radial-gradient(circle at top right,#c0e5fb 0%,#fbecc6 100%) top right,radial-gradient(circle at bottom left,#c0e5fb 0%,#fbecc6 100%) bottom left,radial-gradient(circle at bottom right,#c0e5fb 0%,#fbecc6 100%) bottom right; background-repeat:repeat; color:black; font-family:'Helvetica',sans-serif; font-size:16px;">
 <h1>Participant Info</h1>
 {% if info %}
+<div id="info_div" style="position:absolute;top:5%;left:50%;transform:translate(-50%, -50%);width:50%;height:35%;background-color:white;border-radius:5px;"></div>
 <p>Id : {{ info['Id'] }}</p>
 <p>Surname : {{ info['Surname'] }}</p>
 <p>Name : {{ info['Name'] }}</p>
 <p>Age : {{ info['Age'] }}</p>
+<p>Attendance Status : {{ info['Present'] }}</p>
+</div>
 {% else %}
 <p>No participant found.</p>
 {% endif %}
