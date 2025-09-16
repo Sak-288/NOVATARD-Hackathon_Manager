b966e5089616a9ed93ef9924b66343a06d5d549e
Amine Sakoute <aminesakoute288@gmail.com>
2025-09-16 19:53:58 +0100

    took the port from railway

--- server.py
+++ server.py
@@ -53,5 +53,5 @@ def home():
     return render_template_string(HTML_TEMPLATE, info=html_data)
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
